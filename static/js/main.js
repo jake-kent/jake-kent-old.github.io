9dde0596578609e41569b123015147efe2ae68e0
Jacob Kent <kentjakel@wustl.edu>
2016-04-01 15:30:56 -0500

    added js scroll tracking, navbar that appears on scroll past welcome pane

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,12 @@
-$("#contact-form").ready(function(){
-	$('.freebirdFormviewerViewNavigationHeaderButton').css('display','none');
-});
+$(document).ready(function(){
+	$(window).scroll(function(){
+		const position = $(window).scrollTop();
+		const aboutpane = $('#about-pane-wrapper').offset().top - 80;
+
+		if (position > aboutpane) {
+			$('#site_navbar').addClass("navbar-fixed").removeClass("navbar-hidden");
+		} else {
+			$('#site_navbar').addClass("navbar-hidden").removeClass("navbar-fixed");
+		}
+	})
+})
