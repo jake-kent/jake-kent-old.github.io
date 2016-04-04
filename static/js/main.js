c353951f8c33ee8c42fcc96c669c2003225d75d1
Jacob Kent <kentjakel@wustl.edu>
2016-04-04 11:55:44 -0500

    added dynamic color to site navbar based on what pane is being viewed

--- static/js/main.js
+++ static/js/main.js
@@ -2,11 +2,20 @@ $(document).ready(function(){
 	$(window).scroll(function(){
 		const position = $(window).scrollTop();
 		const aboutpane = $('#about-pane-wrapper').offset().top - 80;
+		const skillexppane = $('#experience-skills-pane').offset().top - 80;
+		const contactpane = $('#contact-pane').offset().top - 80;
 
-		if (position > aboutpane) {
-			$('#site_navbar').addClass("navbar-fixed").removeClass("navbar-hidden");
-		} else {
-			$('#site_navbar').addClass("navbar-hidden").removeClass("navbar-fixed");
+		if (position > contactpane){
+			$('#site_navbar').addClass("navbar-fixed navbar-orange").removeClass("navbar-hidden navbar-red");
+		}
+		else if (position > skillexppane && position <= contactpane) {
+			$('#site_navbar').addClass("navbar-fixed navbar-red").removeClass("navbar-hidden navbar-white navbar-orange");
+		} 
+		else if (position > aboutpane && position <= skillexppane) {
+			$('#site_navbar').addClass("navbar-fixed navbar-white").removeClass("navbar-hidden navbar-red");
+		}
+		else {
+			$('#site_navbar').addClass("navbar-hidden").removeClass("navbar-fixed navbar-white");
 		}
 	})
 })
