0bec266c06fc9f68ea97994a91b1655cb1ba5b4f
Jacob Kent <kentjakel@wustl.edu>
2016-05-12 18:13:11 -0500

    added archhacks link to site header, fixed menu visibility on page reload

--- static/js/main.js
+++ static/js/main.js
@@ -19,10 +19,37 @@ $(document).ready(function(){
 			audio_playing = true;
 		}
 	});
-	const position = $(window).scrollTop();
-	const aboutpane = $('#about-pane-wrapper').offset().top - 80;
-	const skillexppane = $('#experience-skills-pane').offset().top - 80;
-	const contactpane = $('#contact-pane').offset().top - 80;
+	var position = $(window).scrollTop();
+	var aboutpane = $('#about-pane-wrapper').offset().top - 80;
+	var skillexppane = $('#experience-skills-pane').offset().top - 80;
+	var contactpane = $('#contact-pane').offset().top - 80;
+
+	if (position > contactpane){
+		$('#site_navbar').addClass("navbar-fixed navbar-orange").removeClass("navbar-hidden navbar-red navbar-white");
+		$('#navbar-contact').addClass("selected");
+		$('#navbar-skillexp').removeClass("selected");
+
+	}
+	else if (position > skillexppane && position <= contactpane) {
+		$('#site_navbar').addClass("navbar-fixed navbar-red").removeClass("navbar-hidden navbar-white navbar-orange");
+		$('#navbar-skillexp').addClass("selected");
+		$('#navbar-contact').removeClass("selected");
+		$('#navbar-about').removeClass("selected");
+	} 
+	else if (position > aboutpane && position <= skillexppane) {
+		$("#player-title").text('');
+		$("#player-title").hide("slide", { direction: "right" }, 500);
+		$("#site-audio-pp").hide("slide", { direction: "right" }, 500);
+		$('#site_navbar').addClass("navbar-fixed navbar-white").removeClass("navbar-hidden navbar-red navbar-orange");
+		$('#navbar-about').addClass("selected");
+		$('#navbar-skillexp').removeClass("selected");
+	}
+	else {
+		$("#player-title").text('Some music while you browse?');
+		$("#player-title").show();
+		$("#site-audio-pp").show("slide", { direction: "right" }, 200);
+		$('#site_navbar').addClass("navbar-hidden").removeClass("navbar-fixed navbar-white navbar-orange navbar-red");
+	}
 	
 	$('#navbar-welcome').click(function(){
 		$(window).scrollTop(0);
@@ -48,12 +75,12 @@ $(document).ready(function(){
 		$('#navbar-skillexp').removeClass("selected");
 		$('#navbar-contact').addClass("selected");
 	});
-  
+	
 	$(window).scroll(function(){
-		const position = $(window).scrollTop();
-		const aboutpane = $('#about-pane-wrapper').offset().top - 80;
-		const skillexppane = $('#experience-skills-pane').offset().top - 80;
-		const contactpane = $('#contact-pane').offset().top - 80;
+		position = $(window).scrollTop();
+		aboutpane = $('#about-pane-wrapper').offset().top - 80;
+		skillexppane = $('#experience-skills-pane').offset().top - 80;
+		contactpane = $('#contact-pane').offset().top - 80;
 
 		if (position > contactpane){
 			$('#site_navbar').addClass("navbar-fixed navbar-orange").removeClass("navbar-hidden navbar-red navbar-white");
@@ -82,4 +109,10 @@ $(document).ready(function(){
 			$('#site_navbar').addClass("navbar-hidden").removeClass("navbar-fixed navbar-white navbar-orange navbar-red");
 		}
 	})
+	$(window).resize(function(){
+		position = $(window).scrollTop();
+		aboutpane = $('#about-pane-wrapper').offset().top - 80;
+		skillexppane = $('#experience-skills-pane').offset().top - 80;
+		contactpane = $('#contact-pane').offset().top - 80;
+	});
 })
